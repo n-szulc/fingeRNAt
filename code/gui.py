c0b9bd1a09ed8d4bff0ee589dda15f272f2a6869
Natalia Szulc <nszulc@iimcb.gov.pl>
2021-07-31 13:14:30 +0200

    GUI custom interactions handling

--- code/gui.py
+++ code/gui.py
@@ -1,7 +1,7 @@
 
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-import subprocess
+import subprocess, os
 
 f1 = None # RNA/DNA file path
 f2 = None # ligands file path
@@ -30,17 +30,18 @@ def run_script():
         addH = "" if option4.get() == "No" else "-addH %s" %option4.get()
         h2o = "" if optionwater.get() == "No" else "-h2o"
         detail = "" if optiondetail.get() == "No" else "-detail"
+        yaml = "" if len(yaml_entry.get()) == 0 else "-custom %s" %yaml_entry.get()
         out = "" if len(entry.get()) == 0 else "-o %s" %entry.get()
 
         if f2 is None:
-            subprocess.call('python fingeRNAt.py -r %s -f %s %s %s %s %s %s %s' % (f1, option.get(), dha, wrapper, addH, out, h2o, detail), shell = True)
+            subprocess.call('python %s/fingeRNAt.py -r %s -f %s %s %s %s %s %s %s' % (os.path.dirname(os.path.realpath(__file__)), f1, option.get(), dha, wrapper, addH, out, h2o, detail), shell = True)
         else:
-            subprocess.call('python fingeRNAt.py -r %s -l %s -f %s %s %s %s %s %s %s' % (f1, f2, option.get(), dha, wrapper, addH, out, h2o, detail), shell = True)
+            subprocess.call('python %s/fingeRNAt.py -r %s -l %s -f %s %s %s %s %s %s %s %s' % (os.path.dirname(os.path.realpath(__file__)), f1, f2, option.get(), dha, wrapper, addH, out, h2o, detail, yaml), shell = True)
 
 root = tk.Tk()
 root.title("fingeRNAt")
-root.geometry("575x880")
-root.resizable(width = False, height = False)
+root.geometry("575x900")
+root.resizable(width = False, height = True)
 
 # Intro
 
@@ -56,7 +57,7 @@ intro2.grid(row = 1, column = 0, sticky = tk.W + tk.E, columnspan = 2)
 # Input files choice
 
 app1 = tk.Frame(root)
-app1.grid(row = 1, column=  0, pady = 10)
+app1.grid(row = 1, column=  0, pady = 7)
 
 lbl1 = tk.Label(app1, text = "Choose path to RNA/DNA file")
 lbl1.grid(row = 0, column = 0, sticky = tk.W )
@@ -79,7 +80,7 @@ browse_bttn2.grid(row = 1, column = 1)
 # Choose SIFt type
 
 app2 = tk.Frame(root)
-app2.grid(row = 2, column = 0, pady = 10)
+app2.grid(row = 2, column = 0, pady = 7)
 
 lbl3 = tk.Label(app2, text = "Choose Structural Interaction Fingerprint (SIFt) type")
 lbl3.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -101,7 +102,7 @@ choicebttn3.grid(row=2, column=1, sticky = tk.W)
 # How to add hydrogens to ligands structures
 
 app4 = tk.Frame(root)
-app4.grid(row = 3, column = 0, pady = 10)
+app4.grid(row = 3, column = 0, pady = 7)
 
 lbl4 = tk.Label(app4, text = "Choose module to add hydrogens to ligands")
 lbl4.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -123,7 +124,7 @@ choicebttn4_2.grid(row = 2, column = 1, sticky = tk.W)
 
 # Consider dha?
 app3_1 = tk.Frame(root)
-app3_1.grid(row = 4, column = 0, pady = 10)
+app3_1.grid(row = 4, column = 0, pady = 7)
 
 lbl3_1 = tk.Label(app3_1, text = "Consider Donor - Hydrogen - Acceptor angle when detecting hydrogen bonds?")
 lbl3_1.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -145,7 +146,7 @@ choicebttn3_12.grid(row = 2, column = 1, sticky = tk.W)
 # Consider H2O?
 
 app3_water = tk.Frame(root)
-app3_water.grid(row = 5, column = 0, pady = 10)
+app3_water.grid(row = 5, column = 0, pady = 7)
 
 lbl3_water = tk.Label(app3_water, text = "Consider water-mediated interactions?")
 lbl3_water.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -166,7 +167,7 @@ choicebttn_water_12.grid(row = 2, column = 1, sticky = tk.W)
 # Wrapper
 
 app3_2 = tk.Frame(root)
-app3_2.grid(row = 6, column = 0, pady = 10)
+app3_2.grid(row = 6, column = 0, pady = 7)
 
 lbl3_2 = tk.Label(app3_2, text = "Choose wrapper")
 lbl3_2.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -193,7 +194,7 @@ choicebttn3_23.grid(row = 2, column = 1, sticky = tk.W)
 # Save -detail?
 
 app_detail = tk.Frame(root)
-app_detail.grid(row = 7, column = 0, pady = 10)
+app_detail.grid(row = 7, column = 0, pady = 7)
 
 lbl3_detail = tk.Label(app_detail, text = "Save details about interactions?")
 lbl3_detail.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
@@ -211,19 +212,32 @@ choicebttn_detail_12.grid(row = 2, column = 1, sticky = tk.W)
 # Optional filename
 
 app3 = tk.Frame(root)
-app3.grid(row = 8, column = 0, pady = 10)
+app3.grid(row = 8, column = 0, pady = 7)
 
 lbl4 = tk.Label(app3, text = "Optional output file path")
 lbl4.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
 entry = tk.Entry(app3,width=50)
 entry.grid(row = 1, column = 0, columnspan = 2, sticky = tk.W + tk.E)
 
+# Optional run fingerDISt directly on generated SIFts outputs
+
+app_yaml = tk.Frame(root)
+app_yaml.grid(row = 9, column = 0, pady = 5)
+
+lbl_yaml = tk.Label(app_yaml, text = "Optional path to YAML file with user-defined interactions")
+lbl_yaml.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W + tk.E)
+lbl_yaml = tk.Label(app_yaml, text = "(Applies only to SIFt type FULL)")
+lbl_yaml.grid(row = 1, column = 0, columnspan = 2, sticky = tk.W + tk.E)
+lbl_yaml.config(font=(None, 12))
+
+yaml_entry = tk.Entry(app_yaml,width=50)
+yaml_entry.grid(row = 2, column = 0, columnspan = 2, sticky = tk.W + tk.E)
 
 
 # Submit button
 
 app5 = tk.Frame(root)
-app5.grid(row = 9, column = 0)
+app5.grid(row = 10, column = 0)
 
 lbl5 = tk.Label(app5, text = "")
 lbl5.grid(row = 0, column = 0)
